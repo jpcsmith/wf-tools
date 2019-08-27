dd9c15c23e9fc5d8bfb556a4ad62daa1f0cc1c00
Jean-Pierre Smith <rougesprit@gmail.com>
2019-08-27 18:15:47 +0200

    Use pandas for timings and interpacket timings

--- lab/classifiers/kfingerprinting.py
+++ lab/classifiers/kfingerprinting.py
@@ -17,110 +17,83 @@ from typing import (
 import numpy as np
 import pandas as pd
 from pandas.core.groupby import DataFrameGroupBy
+from pandas.api.types import CategoricalDtype
 
 
-EncPacket = NamedTuple(
-    'EncPacket', [('timestamp', float), ('size', int), ('incoming', bool)])
-InterarrivalStats = NamedTuple(
-    'InterarrivalStats',
-    [('max', float), ('mean', float), ('std', float), ('upperq', float)])
-# TODO: Preconding: Timestamps start from 0
-Trace = List[EncPacket]
+Directions = CategoricalDtype(['in', 'out', 'both'])
 
 
-def interarrival_times(packets: Iterator[EncPacket]) -> Iterator[float]:
+def interarrival_times(trace: pd.DataFrame) -> pd.DataFrame:
     """Returns the interarrival times between the provided packets."""
-    # Get the first packet
-    previous = next(packets, None)
-
-    if previous is not None:
-        for pkt in packets:
-            yield pkt.timestamp - previous.timestamp
-            previous = pkt
+    return trace['timestamp'].diff().dropna()
 
 
-def _interarrival_stats(packets: Iterable[EncPacket]) -> InterarrivalStats:
-    times = list(interarrival_times(iter(packets)))
-    if not times:
-        return InterarrivalStats(0, 0, 0, 0)
-    return InterarrivalStats(max(times), np.mean(times), np.std(times),
-                             np.percentile(times, 75))
-
-
-def interarrival_stats(trace: Trace) -> Iterator[float]:
+def interarrival_stats(trace: pd.DataFrame) -> pd.DataFrame:
     """Extracts features for the max, mean, standard deviation and
     75th-percentile for the incoming, outgoing and entire trace.
     """
-    return itertools.chain(
-        _interarrival_stats(pkt for pkt in trace if pkt.incoming),
-        _interarrival_stats(pkt for pkt in trace if not pkt.incoming),
-        _interarrival_stats(trace),
-    )
+    assert not trace.empty
+
+    times = trace.groupby('direction').apply(interarrival_times)
+    stats = times.groupby('direction').describe()
+
+    if 'in' not in stats.index:
+        stats = stats.append(pd.Series([0, 0, 0], name='in').describe())
+    if 'out' not in stats.index:
+        stats = stats.append(pd.Series([0, 0, 0], name='out').describe())
+
+    return stats.append(interarrival_times(trace).describe().rename('both'))
 
 
-def _timestamp_percentiles(packets: Iterable[EncPacket]) \
-        -> Tuple[float, float, float, float]:
-    """Returns the 25th, 50th, 75th and 100th percentiles of the timestamps."""
-    times = list(pkt.timestamp for pkt in packets)
-    if not times:
-        return (0, 0, 0, 0)
-    return (np.percentile(times, 25), np.percentile(times, 50),
-            np.percentile(times, 75), np.percentile(times, 100))
-
-
-def timestamp_percentiles(trace: Trace) -> Iterator[float]:
+def timestamp_percentiles(trace: pd.DataFrame) -> pd.DataFrame:
     """Extracts features for 25th to 100th quartiles of timestamps for the
     incoming, outgoing and entire subtraces. Missing subtraces result in values
     of 0.
     """
-    return itertools.chain(
-        _timestamp_percentiles(pkt for pkt in trace if pkt.incoming),
-        _timestamp_percentiles(pkt for pkt in trace if not pkt.incoming),
-        _timestamp_percentiles(trace),
-    )
+    def _quantiles(frame):
+        return frame.quantile([0.25, 0.5, 0.75, 1]).rename('quantile')
+
+    assert not trace.empty
+
+    percentiles = trace.groupby(
+        'direction', observed=True)['timestamp'].apply(_quantiles).unstack()
+    percentiles.index = percentiles.index.astype(Directions)
+
+    if 'in' not in percentiles.index:
+        percentiles.loc['in'] = _quantiles(pd.Series([0, 0, 0, 0]))
+    if 'out' not in percentiles.index:
+        percentiles.loc['out'] = _quantiles(pd.Series([0, 0, 0, 0]))
+    return percentiles.append(_quantiles(trace['timestamp']).rename('both'))
 
 
-def packet_counts(trace: Trace) -> Tuple[int, int, int]:
-    """Returns the number of incoming, outgoing and total number of packets."""
-    return (sum(pkt.incoming for pkt in trace),
-            sum(not pkt.incoming for pkt in trace),
-            len(trace))
-
-
-def head_tail_concentration(trace: Trace, length: int = 30) \
-        -> Tuple[int, int, int, int]:
-    """Returns the number of incoming and outgoing packets in the first and
-    last `length` packets of the trace. The result has the form
-    (# inc. in head, # out. in head, # inc. in tail, # out. in tail)
-    """
-    return (sum(pkt.incoming for pkt in trace[:length]),
-            sum(not pkt.incoming for pkt in trace[:length]),
-            sum(pkt.incoming for pkt in trace[-length:]),
-            sum(not pkt.incoming for pkt in trace[-length:]))
-
-
-def _bin_trace(trace: pd.DataFrame, bin_size: int) -> DataFrameGroupBy:
-    trace['group'] = np.arange(len(trace)) // bin_size
-    return trace.groupby('group')
-
-
-def outgoing_concentration_stats(trace: pd.DataFrame, bin_size: int = 20):
-    """Compute statistics over the number of outgoing packets in each 20 packet
-    interval.
-    """
-    frame = trace.copy()
-    frame['outgoing'] = 1 - frame['incoming']
-    return _bin_trace(frame, bin_size)['outgoing'].sum().describe()
-
-
-def extract_features(trace: Trace) -> np.ndarray:
-    """Extracts and returns the features as defined in the k-fingerprinting
-    paper.
-    """
-    features = [
-        interarrival_stats(trace),
-        timestamp_percentiles(trace),
-        iter(packet_counts(trace)),
-        iter(head_tail_concentration(trace, length=30)),
-    ]
-    return list(itertools.chain.from_iterable(features))
+# def packet_counts(trace: Trace) -> Tuple[int, int, int]:
+#     """Returns the number of incoming, outgoing and total number of packets."""
+#     return (sum(pkt.incoming for pkt in trace),
+#             sum(not pkt.incoming for pkt in trace),
+#             len(trace))
+#
+#
+# def head_tail_concentration(trace: Trace, length: int = 30) \
+#         -> Tuple[int, int, int, int]:
+#     """Returns the number of incoming and outgoing packets in the first and
+#     last `length` packets of the trace. The result has the form
+#     (# inc. in head, # out. in head, # inc. in tail, # out. in tail)
+#     """
+#     return (sum(pkt.incoming for pkt in trace[:length]),
+#             sum(not pkt.incoming for pkt in trace[:length]),
+#             sum(pkt.incoming for pkt in trace[-length:]),
+#             sum(not pkt.incoming for pkt in trace[-length:]))
+#
+#
+# def _bin_trace(trace: pd.DataFrame, bin_size: int) -> DataFrameGroupBy:
+#     trace['group'] = np.arange(len(trace)) // bin_size
+#     return trace.groupby('group')
+#
+#
+# def outgoing_concentration_stats(trace: pd.DataFrame, bin_size: int = 20):
+#     """Compute statistics over the number of outgoing packets in each 20 packet
+#     interval.
+#     """
+#     frame = trace.copy()
+#     frame['outgoing'] = 1 - frame['incoming']
+#     return _bin_trace(frame, bin_size)['outgoing'].sum().describe()
--- tests/classifiers/test_kfingerprinting.py
+++ tests/classifiers/test_kfingerprinting.py
@@ -1,6 +1,5 @@
 """Tests for lab.classifiers.kfingerprinting."""
 # pylint: disable=redefined-outer-name
-from math import sqrt
 from typing import (
     Iterable,
     Tuple,
@@ -9,51 +8,42 @@ from typing import (
 import pytest
 from pytest import approx
 import pandas as pd
+from pandas.api.types import CategoricalDtype
 
 from lab.classifiers.kfingerprinting import (
     interarrival_stats,
     timestamp_percentiles,
-    packet_counts,
-    EncPacket,
-    Trace,
-    head_tail_concentration,
-    outgoing_concentration_stats,
 )
 
 
 @pytest.fixture
 def empty_trace():
     """An empty packet trace."""
-    return []
+    return make_trace([])
 
 
 @pytest.fixture
 def mixed_trace():
     """Returns a trace of both input and output packets."""
-    return [
-        EncPacket(0.0, 1000, incoming=False),
-        EncPacket(0.1, 1500, incoming=True),
-        EncPacket(0.3, 1000, incoming=True),
-        EncPacket(0.6, 900, incoming=False),
-        EncPacket(1.0, 1500, incoming=False),
-        EncPacket(1.5, 100, incoming=True)
-    ]
+    return make_trace([(0.0, 1000, False), (0.1, 1500, True), (0.3, 1000, True),
+                       (0.6, 900, False), (1.0, 1500, False), (1.5, 100, True)])
 
 
 @pytest.fixture
 def outgoing_trace():
     """Returns a trace of both input and output packets."""
-    return [
-        EncPacket(0.0, 1000, incoming=False),
-        EncPacket(0.6, 900, incoming=False),
-        EncPacket(1.0, 1500, incoming=False),
-    ]
+    return make_trace(
+        [(0.0, 1000, False), (0.6, 900, False), (1.0, 1500, False)])
 
 
 def make_trace(packets: Iterable[Tuple[float, int, bool]]) -> pd.DataFrame:
     """Create a dataframe containing the provided packets."""
-    return pd.DataFrame.from_records(
-        packets, columns=['timestamp', 'size', 'incoming'])
+    direction_cat = CategoricalDtype(['in', 'out', 'both'])
+    trace = pd.DataFrame.from_records(
+        packets, columns=['timestamp', 'size', 'direction'])
+    trace['direction'] = trace['direction'].map(
+        {True: 'in', False: 'out'}).astype(direction_cat)
+    return trace
 
 
 @pytest.fixture
@@ -70,19 +60,26 @@ def long_trace():
 class TestInterarrivalStats:
     """Test cases for the interarrival_stats feature set."""
     @staticmethod
-    def test_empty_trace(empty_trace):
-        """It should return a feature set of 12 zeroes."""
-        assert list(interarrival_stats(empty_trace)) == ([0] * 12)
-
-    @staticmethod
     def test_mixed_trace(mixed_trace):
         """It should return the max, mean, std and upper quartiles of the
         interarrival times of the incoming, outgoing and entire trace.
         """
-        stats = list(interarrival_stats(mixed_trace))
-        assert stats[:4] == approx([1.2, 0.7, 0.5, 0.95])
-        assert stats[4:8] == approx([0.6, 0.5, 0.1, 0.55])
-        assert stats[8:] == approx([0.5, 0.3, 0.1414, 0.4], 1e-3)
+        stats = interarrival_stats(mixed_trace)
+
+        assert stats.loc['in', 'max'] == approx(1.2)
+        assert stats.loc['in', 'mean'] == approx(0.7)
+        assert stats.loc['in', 'std'] == approx(0.707, 1e-3)
+        assert stats.loc['in', '75%'] == approx(0.95)
+
+        assert stats.loc['out', 'max'] == approx(0.6)
+        assert stats.loc['out', 'mean'] == approx(0.5)
+        assert stats.loc['out', 'std'] == approx(0.141, 1e-2)
+        assert stats.loc['out', '75%'] == approx(0.55)
+
+        assert stats.loc['both', 'max'] == approx(0.5)
+        assert stats.loc['both', 'mean'] == approx(0.3)
+        assert stats.loc['both', 'std'] == approx(0.158, 1e-2)
+        assert stats.loc['both', '75%'] == approx(0.4)
 
     @staticmethod
     def test_outgoing_trace(outgoing_trace):
@@ -90,28 +87,36 @@ class TestInterarrivalStats:
         outgoing trace and total trace (the same) while leaving the incoming
         stats as 0.
         """
-        stats = list(interarrival_stats(outgoing_trace))
-        assert stats[:4] == [0.0] * 4
-        assert stats[4:8] == approx([0.6, 0.5, 0.1, 0.55])
-        assert stats[8:] == approx([0.6, 0.5, 0.1, 0.55])
+        stats = interarrival_stats(outgoing_trace)
+
+        assert stats.loc['in', 'max'] == 0
+        assert stats.loc['in', 'mean'] == 0
+        assert stats.loc['in', 'std'] == 0
+        assert stats.loc['in', '75%'] == 0
+
+        assert stats.loc['out', 'max'] == approx(0.6)
+        assert stats.loc['out', 'mean'] == approx(0.5)
+        assert stats.loc['out', 'std'] == approx(0.141, 1e-2)
+        assert stats.loc['out', '75%'] == approx(0.55)
+
+        assert stats.loc['both', 'max'] == approx(0.6)
+        assert stats.loc['both', 'mean'] == approx(0.5)
+        assert stats.loc['both', 'std'] == approx(0.141, 1e-2)
+        assert stats.loc['both', '75%'] == approx(0.55)
 
 
 class TestTimestampPercentiles:
     """Test cases for the timestamp_percentile feature set."""
     @staticmethod
-    def test_empty_trace(empty_trace):
-        """It should return a feature set of 12 zeroes."""
-        assert list(timestamp_percentiles(empty_trace)) == ([0] * 12)
-
-    @staticmethod
     def test_mixed_trace(mixed_trace):
         """It should return the max, mean, std and upper quartiles of the
         interarrival times of the incoming, outgoing and entire trace.
         """
-        percentiles = list(timestamp_percentiles(mixed_trace))
-        assert percentiles[:4] == approx([0.2, 0.3, 0.9, 1.5])
-        assert percentiles[4:8] == approx([0.3, 0.6, 0.8, 1])
-        assert percentiles[8:] == approx([0.15, 0.45, 0.9, 1.5], 1e-3)
+        percentiles = timestamp_percentiles(mixed_trace)
+
+        assert list(percentiles.loc['in']) == approx([0.2, 0.3, 0.9, 1.5])
+        assert list(percentiles.loc['out']) == approx([0.3, 0.6, 0.8, 1])
+        assert list(percentiles.loc['both']) == approx([0.15, 0.45, 0.9, 1.5], 1e-3)
 
     @staticmethod
     def test_outgoing_trace(outgoing_trace):
@@ -119,73 +124,75 @@ class TestTimestampPercentiles:
         outgoing trace and total trace (the same) while leaving the incoming
         stats as 0.
         """
-        percentiles = list(timestamp_percentiles(outgoing_trace))
-        assert percentiles[:4] == [0.0] * 4
-        assert percentiles[4:8] == approx([0.3, 0.6, 0.8, 1])
-        assert percentiles[8:] == approx([0.3, 0.6, 0.8, 1])
+        percentiles = timestamp_percentiles(outgoing_trace)
+
+        print(list(percentiles.loc['in']))
+        assert list(percentiles.loc['in']) == [0.0] * 4
+        assert list(percentiles.loc['out']) == approx([0.3, 0.6, 0.8, 1])
+        assert list(percentiles.loc['both']) == approx([0.3, 0.6, 0.8, 1])
 
 
-class TestPacketCounts:
-    """Test cases for the timestamp_percentile feature set."""
-    @staticmethod
-    def test_empty_trace(empty_trace: Trace):
-        """It should return a feature set of 3 zeroes."""
-        assert packet_counts(empty_trace) == (0, 0, 0)
-
-    @staticmethod
-    def test_mixed_trace(mixed_trace: Trace):
-        """It should return the number of packets in the
-        (incoming, outgoing, entire) subtraces.
-        """
-        assert packet_counts(mixed_trace) == (3, 3, 6)
-
-    @staticmethod
-    def test_outgoing_trace(outgoing_trace: Trace):
-        """It should return the number of packets in the
-        (incoming, outgoing, entire) subtraces, with the form
-        (0, outgoing, outgoing) since there are no incoming packets.
-        """
-        assert packet_counts(outgoing_trace) == (0, 3, 3)
-
-
-class TestHeadTailConcentration:
-    """Test cases for the timestamp_percentile feature set."""
-    @staticmethod
-    def test_empty_trace(empty_trace: Trace):
-        """It should return a feature set of 4 zeroes."""
-        assert head_tail_concentration(empty_trace) == (0, 0, 0, 0)
-
-    @staticmethod
-    def test_mixed_trace(mixed_trace: Trace):
-        """It should return the # inc, # out. for the head and tail
-        respectively.
-        """
-        assert head_tail_concentration(mixed_trace, length=3) == (2, 1, 1, 2)
-
-    @staticmethod
-    def test_outgoing_trace(outgoing_trace: Trace):
-        """It should return the number of packets in the
-        (incoming, outgoing, entire) subtraces, with the form
-        (0, outgoing, outgoing) since there are no incoming packets.
-        """
-        assert head_tail_concentration(outgoing_trace, length=2) == (0, 2, 0, 2)
-
-
-class TestOutgoingConcentrationStats:
-    """Test cases for the outgoing_concentration_stats."""
-    @staticmethod
-    def test_empty_trace(empty_trace: Trace):
-        pass
-
-    @staticmethod
-    def test_mixed_trace(long_trace: Trace):
-        """It should compute the summary stats over bins of size 5 from the
-        long trace.
-        """
-        result = outgoing_concentration_stats(long_trace, bin_size=5)
-        print(result)
-        assert result['min'] == 1
-        assert result['max'] == 3
-        assert result['50%'] == 2
-        assert result['mean'] == 2
-        assert result['std'] == 1
+# class TestPacketCounts:
+#     """Test cases for the timestamp_percentile feature set."""
+#     @staticmethod
+#     def test_empty_trace(empty_trace: Trace):
+#         """It should return a feature set of 3 zeroes."""
+#         assert packet_counts(empty_trace) == (0, 0, 0)
+#
+#     @staticmethod
+#     def test_mixed_trace(mixed_trace: Trace):
+#         """It should return the number of packets in the
+#         (incoming, outgoing, entire) subtraces.
+#         """
+#         assert packet_counts(mixed_trace) == (3, 3, 6)
+#
+#     @staticmethod
+#     def test_outgoing_trace(outgoing_trace: Trace):
+#         """It should return the number of packets in the
+#         (incoming, outgoing, entire) subtraces, with the form
+#         (0, outgoing, outgoing) since there are no incoming packets.
+#         """
+#         assert packet_counts(outgoing_trace) == (0, 3, 3)
+#
+#
+# class TestHeadTailConcentration:
+#     """Test cases for the timestamp_percentile feature set."""
+#     @staticmethod
+#     def test_empty_trace(empty_trace: Trace):
+#         """It should return a feature set of 4 zeroes."""
+#         assert head_tail_concentration(empty_trace) == (0, 0, 0, 0)
+#
+#     @staticmethod
+#     def test_mixed_trace(mixed_trace: Trace):
+#         """It should return the # inc, # out. for the head and tail
+#         respectively.
+#         """
+#         assert head_tail_concentration(mixed_trace, length=3) == (2, 1, 1, 2)
+#
+#     @staticmethod
+#     def test_outgoing_trace(outgoing_trace: Trace):
+#         """It should return the number of packets in the
+#         (incoming, outgoing, entire) subtraces, with the form
+#         (0, outgoing, outgoing) since there are no incoming packets.
+#         """
+#         assert head_tail_concentration(outgoing_trace, length=2) == (0, 2, 0, 2)
+#
+#
+# class TestOutgoingConcentrationStats:
+#     """Test cases for the outgoing_concentration_stats."""
+#     @staticmethod
+#     def test_empty_trace(empty_trace: Trace):
+#         pass
+#
+#     @staticmethod
+#     def test_mixed_trace(long_trace: Trace):
+#         """It should compute the summary stats over bins of size 5 from the
+#         long trace.
+#         """
+#         result = outgoing_concentration_stats(long_trace, bin_size=5)
+#         print(result)
+#         assert result['min'] == 1
+#         assert result['max'] == 3
+#         assert result['50%'] == 2
+#         assert result['mean'] == 2
+#         assert result['std'] == 1
