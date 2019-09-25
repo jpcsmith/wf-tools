d43c5d796786ad3141debb54a173e47efdcb70c7
Jean-Pierre Smith <rougesprit@gmail.com>
2019-09-25 17:28:05 +0200

    Test fetcher stop after 3 failures

--- tests/test_website_trace_experiment.py
+++ tests/test_website_trace_experiment.py
@@ -23,7 +23,8 @@ def sniffer():
     """Returns a mocked sniffer."""
     mock = Mock(spec=PacketSniffer)
     type(mock).results = PropertyMock(side_effect=[
-        'capture-A', 'capture-B', 'capture-C', 'capture-D'])
+        'capture-A', 'capture-B', 'capture-C', 'capture-D',
+        'capture-E', 'capture-F', 'capture-G', 'capture-H'])
     return mock
 
 
@@ -63,6 +64,32 @@ def failed_session_factory():
     return mock_factory
 
 
+@pytest.fixture
+def seq_failed_session_factory():
+    """Returns a factory for creating mock sessions which fail after 2
+    successes.
+    """
+    mock_factory = Mock(spec=SessionFactory)
+    mock_sessions = [
+        MagicMock(spec=ChromiumSession, **{
+            'page_source': f'source-{val}',
+            'fetch_page.return_value': f'source-{val}',
+            'performance_log.return_value': f'trace-{val}',
+        }) for val in ['A', 'B']
+    ] + [
+        MagicMock(spec=ChromiumSession, **{
+            'page_source': None,
+            'fetch_page.side_effect': FetchFailed,
+            'performance_log.return_value': f'trace-{val}',
+        }) for val in ['C', 'D', 'E', 'F', 'G', 'H', 'I']
+    ]
+    for mock in mock_sessions:
+        mock.__enter__.return_value = mock
+
+    mock_factory.create.side_effect = mock_sessions
+    return mock_factory
+
+
 def test_sample_domain(sniffer, session_factory):
     """It should yield the samples for QUIC & TCP for domain."""
     domain = Domain('example.com')
@@ -84,8 +111,8 @@ def test_sample_domain(sniffer, session_factory):
     assert result == expected
 
 
-def test_stop_on_failure(sniffer, failed_session_factory):
-    """It should stop the repetitions on failure."""
+def test_initial_failure(sniffer, failed_session_factory):
+    """It should stop the repetitions on an initial failure."""
     domain = Domain('example.com')
     expected = [
         {'domain': domain, 'with_quic': True, 'page_source': None,
@@ -97,5 +124,33 @@ def test_stop_on_failure(sniffer, failed_session_factory):
     experiment = WebsiteTraceExperiment(sniffer, failed_session_factory)
 
     result = list(experiment.sample_domain(domain, repetitions=2))
+
+    assert result == expected
+
+
+def test_repeated_failure(sniffer, seq_failed_session_factory):
+    """It should stop the repetitions on an repeated failures."""
+    domain = Domain('example.com')
+    expected = [
+        # QUIC successes
+        {'domain': domain, 'with_quic': True, 'page_source': 'source-A',
+         'status': 'success', 'http_trace': 'trace-A', 'packets': 'capture-A'},
+        {'domain': domain, 'with_quic': True, 'page_source': 'source-B',
+         'status': 'success', 'http_trace': 'trace-B', 'packets': 'capture-B'},
+        # QUIC failures
+        {'domain': domain, 'with_quic': True, 'page_source': None,
+         'status': 'failure', 'http_trace': 'trace-C', 'packets': 'capture-C'},
+        {'domain': domain, 'with_quic': True, 'page_source': None,
+         'status': 'failure', 'http_trace': 'trace-D', 'packets': 'capture-D'},
+        {'domain': domain, 'with_quic': True, 'page_source': None,
+         'status': 'failure', 'http_trace': 'trace-E', 'packets': 'capture-E'},
+        # TCP failure
+        {'domain': domain, 'with_quic': False, 'page_source': None,
+         'status': 'failure', 'http_trace': 'trace-F', 'packets': 'capture-F'},
+    ]
+
+    experiment = WebsiteTraceExperiment(sniffer, seq_failed_session_factory)
+
+    result = list(experiment.sample_domain(domain, repetitions=10))
 
     assert result == expected
