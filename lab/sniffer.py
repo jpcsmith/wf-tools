b75caeb6746306a6e4808a2876565cb0d70ccac7
Jean-Pierre Smith <rougesprit@gmail.com>
2019-09-26 20:29:37 +0200

    Use editcap to truncate the pcap packets

--- lab/sniffer.py
+++ lab/sniffer.py
@@ -3,6 +3,7 @@ import io
 import time
 import logging
 import threading
+import subprocess
 from typing import Optional
 
 import scapy
@@ -12,68 +13,9 @@ import scapy.sendrecv
 import scapy.utils
 import scapy.layers.inet
 
-# Disable all layers & protocols besides ethernet, IP, TCP and UDP
-# scapy.layers.l2.Ether.payload_guess = [({"type": 0x800}, scapy.layers.inet.IP)]
-# scapy.layers.inet.IP.payload_guess = [
-#     ({"frag": 0, "proto": 0x11}, scapy.layers.inet.UDP),
-#     ({"frag": 0, "proto": 0x06}, scapy.layers.inet.TCP),
-# ]
-# scapy.layers.inet.UDP.payload_guess = []
-# scapy.layers.inet.TCP.payload_guess = []
-
 
 class SnifferStartTimeout(Exception):
     """Raised when the sniffer fails to start due to a timeout."""
-
-
-class TruncatingPcapWriter(scapy.utils.PcapWriter):
-    """A pcap writer which additionally truncated packets to a predefined
-    length before  writing.
-    """
-    def __init__(self, *args, snaplen: Optional[int] = None, **kwargs):
-        assert snaplen is None or snaplen > 0
-        super().__init__(*args, **kwargs)
-        self.snaplen = snaplen
-
-    def write(self, pkt):
-        # We patch out the MTU variable here to ensure the correct value is
-        # written to the PCAP header
-        old_mtu = scapy.utils.MTU
-        try:
-            scapy.utils.MTU = self.snaplen or old_mtu
-            super().write(pkt)
-        finally:
-            scapy.utils.MTU = old_mtu
-
-    # pylint: disable=too-many-arguments
-    def _write_packet(self, packet, sec=None, usec=None, caplen=None,
-                      wirelen=None):
-        """Writes a single packet to the pcap file.
-
-        Operates the same as in PcapWriter, but truncated the packet to
-        snaplen.
-        """
-        if hasattr(packet, "time"):
-            if sec is None:
-                sec = int(packet.time)
-                usec = int(round((packet.time - sec)
-                                 * (1000000000 if self.nano else 1000000)))
-        if usec is None:
-            usec = 0
-
-        assert caplen is None and wirelen is None
-        rawpkt = scapy.compat.raw(packet)
-
-        caplen = len(rawpkt) if self.snaplen is None else self.snaplen
-        wirelen = getattr(packet, "wirelen", None) or len(rawpkt)
-
-        if self.snaplen is not None:
-            rawpkt = rawpkt[:self.snaplen]
-
-        # pylint: disable=protected-access
-        assert caplen and wirelen
-        scapy.utils.RawPcapWriter._write_packet(
-            self, rawpkt, sec=sec, usec=usec, caplen=caplen, wirelen=wirelen)
 
 
 class PacketSniffer:
@@ -104,22 +46,35 @@ class PacketSniffer:
         """Alias for pcap"""
         return self.pcap()
 
+    def _truncate_pcap(self, pcap: bytes) -> bytes:
+        assert self.snaplen is not None and self.snaplen > 0
+        command = ['editcap', '-F', 'pcap', '-s', str(self.snaplen), '-', '-']
+        process = subprocess.Popen(
+            command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate(pcap)
+
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(
+                process.returncode, ' '.join(command), stdout, stderr)
+        return stdout
+
     def pcap(self) -> bytes:
         """Returns the results in pcap format serialised to bytes."""
+        pcap = PacketSniffer.to_pcap(self._sniffer.results)
         if self.snaplen:
             self._logger.info("Truncating packets to %d bytes", self.snaplen)
-        return PacketSniffer.to_pcap(self._sniffer.results, self.snaplen)
+            pcap = self._truncate_pcap(pcap)
+        return pcap
 
     @staticmethod
-    def to_pcap(packet_list: scapy.plist.PacketList,
-                snaplen: Optional[int] = None) -> bytes:
+    def to_pcap(packet_list: scapy.plist.PacketList) -> bytes:
         """Encodes the provided packet list in PCAP format."""
         byte_buffer = io.BytesIO()
-        # with TruncatingPcapWriter(byte_buffer, snaplen=snaplen) as writer:
-        with TruncatingPcapWriter("/tmp/some-pcap.pcap", snaplen=snaplen) as writer:
+        with scapy.utils.PcapWriter(byte_buffer) as writer:
             writer.write(packet_list)
             writer.flush()
-            return b'Dumy'
+            # PcapWriter will close the bytebuffer so we must return in 'with'
             return byte_buffer.getvalue()
 
     def start(self) -> None:
