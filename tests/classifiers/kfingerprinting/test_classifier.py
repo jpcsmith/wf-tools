78e3cc2b78ec28e5cfcf8c1e6f0f93766c95f055
Jean-Pierre Smith <rougesprit@gmail.com>
2020-01-22 12:11:22 +0100

    Skip y_2d tests

--- tests/classifiers/kfingerprinting/test_classifier.py
+++ tests/classifiers/kfingerprinting/test_classifier.py
@@ -1,4 +1,5 @@
 """Tests for the KFingerprintingClassifier."""
+import pytest
 from sklearn.utils.estimator_checks import parametrize_with_checks
 from lab.classifiers.kfingerprinting import KFingerprintingClassifier
 
@@ -6,4 +7,6 @@ from lab.classifiers.kfingerprinting import KFingerprintingClassifier
 @parametrize_with_checks([KFingerprintingClassifier])
 def test_sklearn_compatiblity(estimator, check):
     """Test that the k-fingerprinting classifier is compatible with sklearn."""
+    if check.func.__name__ == 'check_supervised_y_2d':
+        pytest.skip("KFingerprintingClassifier does not support 2d labels.")
     check(estimator)
