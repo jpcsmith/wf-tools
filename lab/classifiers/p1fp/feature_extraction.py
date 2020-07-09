081f79740c5c107cf6ae780544ca3878d526918b
Jean-Pierre Smith <rougesprit@gmail.com>
2020-07-09 11:15:03 +0200

    Make public the function to convert a single trace

--- lab/classifiers/p1fp/feature_extraction.py
+++ lab/classifiers/p1fp/feature_extraction.py
@@ -3,7 +3,13 @@ from typing import Sequence
 import numpy as np
 
 
-def _vectorize_trace(trace: Sequence, n_features: int):
+def vectorize_trace(trace: Sequence, n_features: int):
+    """Vectorize the trace, extracting features used by the p1-FP(C)
+    classifier.
+
+    Traces longer than n_features are truncated, traces shorter are
+    padded with zero.
+    """
     # Truncate the trace to the required length if longer
     trace = trace[:n_features]
 
@@ -15,10 +21,10 @@ def _vectorize_trace(trace: Sequence, n_features: int):
 
 
 def vectorize_traces(traces: Sequence[Sequence], n_features: int) -> np.ndarray:
-    """Vectorize the traces, extracting features used by the p1-FP(C)
+    """Vectorize multiple traces, extracting features used by the p1-FP(C)
     classifier.
 
     Traces longer than n_features are truncated, traces shorter are
     padded with zero.
     """
-    return np.array([_vectorize_trace(trace, n_features) for trace in traces])
+    return np.array([vectorize_trace(trace, n_features) for trace in traces])
