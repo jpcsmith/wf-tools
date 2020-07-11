33a8ed07d784c0b8c117554f305470e315e2c861
Jean-Pierre Smith <rougesprit@gmail.com>
2020-07-11 19:30:58 +0200

    Add the extraction of the interarrival times

--- lab/feature_extraction/trace.py
+++ lab/feature_extraction/trace.py
@@ -45,3 +45,59 @@ def extract_sizes_3d(X, dimension: int = 0) -> np.ndarray:
     assert dimension >= 0
     dimension = dimension or max(len(trace) for trace in X)
     return np.array([extract_sizes(trace, dimension) for trace in X])
+
+
+def extract_interarrival_times(trace, dimension: int = 0) -> np.ndarray:
+    """Extract the interarrival times from the trace.
+
+    Parameters
+    ----------
+    trace : array-like of shape (n_packets, 2)
+        trace is a sequence of n_packets, where each packet stores the
+        timestamp and signed size.
+
+    dimension :
+        If dimension is greater than zero, truncate or pad the resulting
+        feature vector the specified dimension.
+    """
+    assert dimension >= 0
+
+    # Truncate the trace to the required length if longer
+    if dimension > 0:
+        trace = trace[:dimension]
+
+    # A negative padding_len results in no padding being added
+    times = np.array([pkt[0] for pkt in trace])
+    # The times should start from zero
+    assert len(times) == 0 or times[0] == 0
+
+    # Calculate the interarrival times
+    times[1:] = times[1:] - times[:-1]
+
+    # Pad the interarrival times if necessary. Truncated would have already
+    # occured if necessary
+    if dimension > 0 and len(times) != dimension:
+        padded_times = np.zeros(dimension, dtype=times.dtype)
+        padded_times[:len(times)] = times
+        return padded_times
+    return times
+
+
+def extract_interarrival_times_3d(X, dimension: int = 0) -> np.ndarray:
+    """Convenience method around extract_interarrival_times for a
+    sequence of traces.
+
+    Parameters:
+    -----------
+    X : Sequence of array-likes each with shape (n_packets(i), 2)
+        A possibly ragged sequence of traces.
+
+    dimension :
+        If dimension is greater than zero, truncate or pad the resulting
+        feature vector the specified dimension. If not specified, the
+        dimension will be the length of the longest trace in X.
+    """
+    assert dimension >= 0
+    dimension = dimension or max(len(trace) for trace in X)
+    return np.array([extract_interarrival_times(trace, dimension)
+                     for trace in X])
--- tests/feature_extraction/test_trace.py
+++ tests/feature_extraction/test_trace.py
@@ -1,14 +1,18 @@
 """Tests for feature extraction."""
+# pylint: disable=invalid-name
 from typing import Tuple
 
 import pytest
 import numpy as np
 
-from lab.feature_extraction.trace import extract_sizes, extract_sizes_3d
+from lab.feature_extraction.trace import (
+    extract_sizes, extract_sizes_3d, extract_interarrival_times,
+    extract_interarrival_times_3d
+)
 
 
 @pytest.fixture(name="sample_traces")
-def fixture_sample_traces() -> Tuple[list, np.ndarray]:
+def fixture_sample_traces() -> Tuple[list, np.ndarray, np.ndarray]:
     """Returns a simple list of traces."""
     traces = [
         [(0, 1350), (0.01, 1350), (0.02, -600), (0.03, 70)],
@@ -20,49 +24,107 @@ def fixture_sample_traces() -> Tuple[list, np.ndarray]:
         [1300, 1350, 1200, 0],
         [1200, 1350, 0, 0]
     ])
-    return traces, expected_features
+    expected_interarrivals = np.array([
+        [0, 0.010, 0.01, 0.01],
+        [0, 0.015, 0.01, 0.00],
+        [0, 0.020, 0.00, 0.00]
+    ])
+    return traces, expected_features, expected_interarrivals
+
+
+def test_extract_interarrival_times_pad():
+    """It should pad the extracted interarrival times to the specified
+    dimension.
+    """
+    np.testing.assert_allclose(
+        extract_interarrival_times(
+            [(0, 1350), (0.01, 1350), (0.02, -600), (0.03, 70)], dimension=4),
+        [0, 0.01, 0.01, 0.01])
+    np.testing.assert_allclose(
+        extract_interarrival_times([(0, 1300), (0.015, 1350), (0.025, 1200)],
+                                   dimension=4),
+        [0, 0.015, 0.01, 0.00])
+    np.testing.assert_allclose(
+        extract_interarrival_times(
+            [(0, 1200), (0.02, 1350)], dimension=4), [0, 0.02, 0.00, 0.00])
+
+
+def test_extract_interarrival_times():
+    """It should extract the interarrival times from the traces."""
+    np.testing.assert_allclose(
+        extract_interarrival_times(
+            [(0, 1350), (0.01, 1350), (0.02, -600), (0.03, 70)]
+        ), [0, 0.01, 0.01, 0.01])
+    np.testing.assert_allclose(
+        extract_interarrival_times([(0, 1300), (0.015, 1350), (0.025, 1200)]),
+        [0, 0.015, 0.01])
+    np.testing.assert_allclose(
+        extract_interarrival_times([(0, 1200), (0.02, 1350)]), [0, 0.02])
+
+
+def test_extract_interarrival_times_3d(sample_traces):
+    """It should extract the interarrival times from the traces
+    and pad to the specified dimension.
+    """
+    traces, _, expected_times = sample_traces
+
+    np.testing.assert_allclose(
+        extract_interarrival_times_3d(traces, dimension=4), expected_times)
+
+
+def test_extract_interarrival_times_3d_truncate(sample_traces):
+    """It should extract the interarrival times from the traces
+    and truncate them to the specified dimension.
+    """
+    traces, _, expected_times = sample_traces
+
+    np.testing.assert_allclose(
+        extract_interarrival_times_3d(traces, dimension=2),
+        expected_times[:, :2])
 
 
 def test_extract_sizes_single_trace():
     """It should extract the sizes for a single trace."""
-    assert np.array_equal(
+    np.testing.assert_array_equal(
         extract_sizes([(0, 1350), (0.01, 1350), (0.02, -600), (0.03, 70)]),
         [1350, 1350, -600, 70])
-    assert np.array_equal(
+    np.testing.assert_array_equal(
         extract_sizes([(0, 1300), (0.015, 1350), (0.025, 1200)]),
         [1300, 1350, 1200])
-    assert np.array_equal(
+    np.testing.assert_array_equal(
         extract_sizes([(0, 1200), (0.02, 1350)]),
         [1200, 1350])
 
 
 def test_extract_sizes_pad(sample_traces):
     """It should pad traces to the specified dimension."""
-    traces, features = sample_traces
+    traces, features, *_ = sample_traces
     features = np.concatenate(
         (features, np.zeros((features.shape[0], 2))), axis=1)
 
     for trace, expected in zip(traces, features):
-        assert np.array_equal(extract_sizes(trace, dimension=6), expected)
+        np.testing.assert_array_equal(
+            extract_sizes(trace, dimension=6), expected)
 
 
 def test_extract_sizes_truncate(sample_traces):
     """It should truncate traces to the specified dimension."""
-    traces, features = sample_traces
+    traces, features, *_ = sample_traces
     for trace, expected in zip(traces, features[:, :2]):
-        assert np.array_equal(extract_sizes(trace, dimension=2), expected)
+        np.testing.assert_array_equal(
+            extract_sizes(trace, dimension=2), expected)
 
 
 def test_extract_sizes_3d(sample_traces):
     """It should work with matices of traces as well.
     """
-    traces, features = sample_traces
-    assert np.array_equal(extract_sizes_3d(traces, dimension=3),
-                          features[:, :3])
+    traces, features, *_ = sample_traces
+    np.testing.assert_array_equal(
+        extract_sizes_3d(traces, dimension=3), features[:, :3])
 
 
 def test_extract_sizes_3d_infer(sample_traces):
     """It should infer the dimension if not specified.
     """
-    traces, features = sample_traces
-    assert np.array_equal(extract_sizes_3d(traces), features)
+    traces, features, *_ = sample_traces
+    np.testing.assert_array_equal(extract_sizes_3d(traces), features)
