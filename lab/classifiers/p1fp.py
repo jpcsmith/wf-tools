52c692dcda256db7946ad6d6ed892ec285640132
Jean-Pierre Smith <rougesprit@gmail.com>
2020-07-17 00:54:52 +0200

    Conditionally use tflearn classifier

--- lab/classifiers/p1fp.py
+++ lab/classifiers/p1fp.py
@@ -12,14 +12,19 @@ The original can be found at https://github.com/seeunoh2/pFP
 """
 import numpy as np
 import tensorflow as tf
-import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.normalization import local_response_normalization
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import OneHotEncoder
+try:
+    import tflearn
+    from tflearn.layers.core import input_data, dropout, fully_connected
+    from tflearn.layers.conv import conv_2d, max_pool_2d
+    from tflearn.layers.normalization import local_response_normalization
+except ImportError:
+    USE_TFLEARN = False
+else:
+    USE_TFLEARN = True
 
 from tensorflow.compat.v1 import keras, nn
 from tensorflow.compat.v1.keras import layers
@@ -67,103 +72,106 @@ class KerasP1FPClassifierC(KerasClassifier):
             n_classes=n_classes, **kwargs)
 
 
-class P1FPClassifierC(BaseEstimator, ClassifierMixin):
-    """p1-FP(C) classifier using a convolution neural network.
-    """
-    def __init__(self, n_epoch: int = 40, snapshot_step: int = 100):
-        self.n_epoch = n_epoch
-        self.snapshot_step = snapshot_step
-
-    def fit(self, X, y, name: str = "p1-fp(C)") -> "P1FPClassifierC":
-        """Fit the convolution neural network with the given data."""
-        X = check_array(X, accept_sparse=False)
-        y = check_array(y, accept_sparse=False, ensure_2d=False)
-
-        n_features = X.shape[1]
-        n_classes = np.unique(y).size
-
-        X = X.reshape([-1, 1, n_features, 1])
-
-        encoder = OneHotEncoder(sparse=False)
-        y = encoder.fit_transform(y.reshape(-1, 1))
-
-        tf.compat.v1.reset_default_graph()
-        network = input_data(shape=[None, 1, n_features, 1], name='input')
-        network = conv_2d(network, 128, 12, activation='relu', regularizer="L2")
-        network = max_pool_2d(network, 10)
-        network = local_response_normalization(network)
-
-        network = conv_2d(network, 128, 12, activation='relu', regularizer="L2")
-        network = max_pool_2d(network, 10)
-        network = local_response_normalization(network)
-
-        network = fully_connected(network, 256, activation='tanh')
-        network = dropout(network, 0.8)
-
-        softmax = fully_connected(network, n_classes, activation='softmax')
-
-        sgd = tflearn.SGD(learning_rate=0.05, lr_decay=0.96, decay_step=1000)
-        top_k = tflearn.metrics.Top_k(3)
-        network = tflearn.regression(
-            softmax, optimizer=sgd, loss='categorical_crossentropy',
-            name='target', metric=top_k)
-
-        # pylint: disable=attribute-defined-outside-init
-        self.n_features_ = n_features
-        self.classes_ = encoder.categories_[0]
-        self.model_ = tflearn.DNN(network, tensorboard_verbose=0)
-
-        self.model_.fit({'input': X}, {'target': y}, validation_set=0.1,
-                        n_epoch=self.n_epoch, snapshot_step=self.snapshot_step,
-                        run_id=name)
-        return self
-
-    def save_model(self, path: str) -> "P1FPClassifierC":
-        """Save the model to the specified path."""
-        check_is_fitted(self, ["model_"])
-        self.model_.save(path)
-        return self
-
-    def predict_proba(self, X):
-        """Make probability predictions for the provided data."""
-        check_is_fitted(self, ["model_", "n_features_"])
-
-        X = check_array(
-            X, accept_sparse=False, ensure_min_features=self.n_features_)
-        X = X.reshape([-1, 1, self.n_features_, 1])
-        return self.model_.predict(X)
-
-    def predict(self, X):
-        """Predict the labels for provided test data X."""
-        check_is_fitted(self, ["classes_"])
-        result = self.predict_label(X)
-        return np.array([self.classes_[ranking[0]] for ranking in result])
-
-    def predict_label(self, X):
-        """Returns the rankings of the labels (indexes in classes_)
-        based on their probabilities.
+if USE_TFLEARN:
+    class P1FPClassifierC(BaseEstimator, ClassifierMixin):
+        """p1-FP(C) classifier using a convolution neural network.
         """
-        check_is_fitted(self, ["model_", "n_features_"])
+        def __init__(self, n_epoch: int = 40, snapshot_step: int = 100):
+            self.n_epoch = n_epoch
+            self.snapshot_step = snapshot_step
 
-        X = check_array(
-            X, accept_sparse=False, ensure_min_features=self.n_features_)
-        X = X.reshape([-1, 1, self.n_features_, 1])
-        return self.model_.predict_label(X)
+        def fit(self, X, y, name: str = "p1-fp(C)") -> "P1FPClassifierC":
+            """Fit the convolution neural network with the given data."""
+            X = check_array(X, accept_sparse=False)
+            y = check_array(y, accept_sparse=False, ensure_2d=False)
 
+            n_features = X.shape[1]
+            n_classes = np.unique(y).size
 
-def onehot(label_array, n_classes):
-    """Perform a one-hot encoding. Taken from the original repo."""
-    converted_arr = np.zeros(shape=(len(label_array), n_classes))
-    i = 0
-    neg = 0
-    for label in label_array:
-        if int(label) == -1:
-            neg += 1
-        converted_arr[i][int(label)] = 1
-        i += 1
-    count = 0
+            X = X.reshape([-1, 1, n_features, 1])
 
-    for item in converted_arr:
-        if item[-1] == 1:
-            count += 1
-    return converted_arr
+            encoder = OneHotEncoder(sparse=False)
+            y = encoder.fit_transform(y.reshape(-1, 1))
+
+            tf.compat.v1.reset_default_graph()
+            network = input_data(shape=[None, 1, n_features, 1], name='input')
+            network = conv_2d(network, 128, 12, activation='relu',
+                              regularizer="L2")
+            network = max_pool_2d(network, 10)
+            network = local_response_normalization(network)
+
+            network = conv_2d(network, 128, 12, activation='relu',
+                              regularizer="L2")
+            network = max_pool_2d(network, 10)
+            network = local_response_normalization(network)
+
+            network = fully_connected(network, 256, activation='tanh')
+            network = dropout(network, 0.8)
+
+            softmax = fully_connected(network, n_classes, activation='softmax')
+
+            sgd = tflearn.SGD(learning_rate=0.05, lr_decay=0.96,
+                              decay_step=1000)
+            top_k = tflearn.metrics.Top_k(3)
+            network = tflearn.regression(
+                softmax, optimizer=sgd, loss='categorical_crossentropy',
+                name='target', metric=top_k)
+
+            # pylint: disable=attribute-defined-outside-init
+            self.n_features_ = n_features
+            self.classes_ = encoder.categories_[0]
+            self.model_ = tflearn.DNN(network, tensorboard_verbose=0)
+
+            self.model_.fit({'input': X}, {'target': y}, validation_set=0.1,
+                            n_epoch=self.n_epoch,
+                            snapshot_step=self.snapshot_step, run_id=name)
+            return self
+
+        def save_model(self, path: str) -> "P1FPClassifierC":
+            """Save the model to the specified path."""
+            check_is_fitted(self, ["model_"])
+            self.model_.save(path)
+            return self
+
+        def predict_proba(self, X):
+            """Make probability predictions for the provided data."""
+            check_is_fitted(self, ["model_", "n_features_"])
+
+            X = check_array(
+                X, accept_sparse=False, ensure_min_features=self.n_features_)
+            X = X.reshape([-1, 1, self.n_features_, 1])
+            return self.model_.predict(X)
+
+        def predict(self, X):
+            """Predict the labels for provided test data X."""
+            check_is_fitted(self, ["classes_"])
+            result = self.predict_label(X)
+            return np.array([self.classes_[ranking[0]] for ranking in result])
+
+        def predict_label(self, X):
+            """Returns the rankings of the labels (indexes in classes_)
+            based on their probabilities.
+            """
+            check_is_fitted(self, ["model_", "n_features_"])
+
+            X = check_array(
+                X, accept_sparse=False, ensure_min_features=self.n_features_)
+            X = X.reshape([-1, 1, self.n_features_, 1])
+            return self.model_.predict_label(X)
+
+    def onehot(label_array, n_classes):
+        """Perform a one-hot encoding. Taken from the original repo."""
+        converted_arr = np.zeros(shape=(len(label_array), n_classes))
+        i = 0
+        neg = 0
+        for label in label_array:
+            if int(label) == -1:
+                neg += 1
+            converted_arr[i][int(label)] = 1
+            i += 1
+        count = 0
+
+        for item in converted_arr:
+            if item[-1] == 1:
+                count += 1
+        return converted_arr
--- tests/classifiers/test_p1fp.py
+++ tests/classifiers/test_p1fp.py
@@ -2,22 +2,29 @@
 import pytest
 import numpy as np
 
-from lab.classifiers.p1fp import P1FPClassifierC, KerasP1FPClassifierC
+from lab.classifiers.p1fp import KerasP1FPClassifierC
+try:
+    from lab.classifiers.p1fp import P1FPClassifierC
+except ImportError:
+    USE_TFLEARN = False
+else:
+    USE_TFLEARN = True
 
 
-@pytest.mark.slow
-def test_p1fpclassifierc(train_test_sizes):
-    """Test that it performs classification."""
-    x_train, x_test, y_train, y_test = train_test_sizes
-    classifier = P1FPClassifierC(n_epoch=1)
-    classifier.fit(x_train, y_train)
+if USE_TFLEARN:
+    @pytest.mark.slow
+    def test_p1fpclassifierc(train_test_sizes):
+        """Test that it performs classification."""
+        x_train, x_test, y_train, y_test = train_test_sizes
+        classifier = P1FPClassifierC(n_epoch=1)
+        classifier.fit(x_train, y_train)
 
-    prediction = classifier.predict(x_test)
-    assert prediction.shape == (y_test.size, )
-    assert np.all(np.isin(np.unique(prediction), np.unique(y_train)))
+        prediction = classifier.predict(x_test)
+        assert prediction.shape == (y_test.size, )
+        assert np.all(np.isin(np.unique(prediction), np.unique(y_train)))
 
-    prob_prediction = classifier.predict_proba(x_test)
-    assert prob_prediction.shape == (y_test.size, np.unique(y_test).size)
+        prob_prediction = classifier.predict_proba(x_test)
+        assert prob_prediction.shape == (y_test.size, np.unique(y_test).size)
 
 
 @pytest.mark.slow
