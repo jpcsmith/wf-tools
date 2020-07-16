3fa4ecf081edf6f27fa06fba897d8b640e0b10f9
Jean-Pierre Smith <rougesprit@gmail.com>
2020-07-17 00:25:44 +0200

    Only create scapy code if installed.

--- lab/sniffer.py
+++ lab/sniffer.py
@@ -14,20 +14,25 @@ import tempfile
 import subprocess
 from subprocess import CompletedProcess, CalledProcessError
 
-import scapy
-import scapy.compat
-import scapy.plist
-import scapy.sendrecv
-import scapy.utils
-from scapy.layers import inet, l2
+try:
+    import scapy
+    import scapy.compat
+    import scapy.plist
+    import scapy.sendrecv
+    import scapy.utils
+    from scapy.layers import inet, l2
 
-# Enable parsing only to the level of UDP and TCP packets
-l2.Ether.payload_guess = [({"type": 0x800}, inet.IP)]
-inet.IP.payload_guess = [
-    ({"frag": 0, "proto": 0x11}, inet.UDP), ({"proto": 0x06}, inet.TCP)
-]
-inet.UDP.payload_guess = []
-inet.TCP.payload_guess = []
+    # Enable parsing only to the level of UDP and TCP packets
+    l2.Ether.payload_guess = [({"type": 0x800}, inet.IP)]
+    inet.IP.payload_guess = [
+        ({"frag": 0, "proto": 0x11}, inet.UDP), ({"proto": 0x06}, inet.TCP)
+    ]
+    inet.UDP.payload_guess = []
+    inet.TCP.payload_guess = []
+except ImportError:
+    USE_SCAPY = False
+else:
+    USE_SCAPY = True
 
 
 class SnifferStartTimeout(Exception):
@@ -54,100 +59,104 @@ class PacketSniffer(abc.ABC):
         """Stop capturing packets."""
 
 
-class ScapyPacketSniffer(PacketSniffer):
-    """Class for capturing network traffic."""
-    stop_delay = 1
+if USE_SCAPY:
+    class ScapyPacketSniffer(PacketSniffer):
+        """Class for capturing network traffic."""
+        stop_delay = 1
 
-    def __init__(self, capture_filter: str = 'tcp or udp',
-                 snaplen: Optional[int] = None, **kwargs):
-        def _started_callback():
+        def __init__(self, capture_filter: str = 'tcp or udp',
+                     snaplen: Optional[int] = None, **kwargs):
+            def _started_callback():
+                with self._start_condition:
+                    self._started = True
+                    self._start_condition.notify_all()
+
+            self._logger = logging.getLogger(__name__)
+            self._filter = capture_filter
+            self._start_condition = threading.Condition()
+            self._started = False
+            self._sniffer = scapy.sendrecv.AsyncSniffer(
+                filter=capture_filter,
+                started_callback=_started_callback,
+                promisc=False,
+                **kwargs,
+            )
+
+            self.snaplen = snaplen
+
+        def _truncate_pcap(self, pcap: bytes) -> bytes:
+            assert self.snaplen is not None and self.snaplen > 0
+            command = ['editcap', '-F', 'pcap',
+                       '-s', str(self.snaplen), '-', '-']
+            process = subprocess.Popen(
+                command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate(pcap)
+
+            if process.returncode != 0:
+                raise subprocess.CalledProcessError(
+                    process.returncode, ' '.join(command), stdout, stderr)
+            return stdout
+
+        def pcap(self) -> bytes:
+            """Returns the results in pcap format serialised to bytes."""
+            pcap = ScapyPacketSniffer.to_pcap(self._sniffer.results)
+            if self.snaplen:
+                self._logger.info("Truncating packets to %d bytes",
+                                  self.snaplen)
+                pcap = self._truncate_pcap(pcap)
+            return pcap
+
+        @staticmethod
+        def to_pcap(packet_list: scapy.plist.PacketList) -> bytes:
+            """Encodes the provided packet list in PCAP format."""
+            byte_buffer = io.BytesIO()
+            with scapy.utils.PcapWriter(byte_buffer) as writer:
+                writer.write(packet_list)
+                writer.flush()
+                # PcapWriter will close the bytebuffer so must return in 'with'
+                return byte_buffer.getvalue()
+
+        def start(self) -> None:
+            """Start capturing packets."""
             with self._start_condition:
-                self._started = True
-                self._start_condition.notify_all()
+                self._sniffer.start()
+                notified = self._start_condition.wait_for(lambda: self._started,
+                                                          timeout=5)
 
-        self._logger = logging.getLogger(__name__)
-        self._filter = capture_filter
-        self._start_condition = threading.Condition()
-        self._started = False
-        self._sniffer = scapy.sendrecv.AsyncSniffer(
-            filter=capture_filter,
-            started_callback=_started_callback,
-            promisc=False,
-            **kwargs,
-        )
+            if not notified:
+                raise SnifferStartTimeout()
+            self._logger.info('Began sniffing for traffic with filter "%s"',
+                              self._filter)
 
-        self.snaplen = snaplen
+        def stop(self) -> None:
+            """Stop capturing packets."""
+            self._logger.info('Waiting %.2fs for sniffer to flush',
+                              self.stop_delay)
+            time.sleep(self.stop_delay)
 
-    def _truncate_pcap(self, pcap: bytes) -> bytes:
-        assert self.snaplen is not None and self.snaplen > 0
-        command = ['editcap', '-F', 'pcap', '-s', str(self.snaplen), '-', '-']
-        process = subprocess.Popen(
-            command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate(pcap)
+            try:
+                self._sniffer.stop()
+            except OSError as err:
+                if err.errno != 9:
+                    raise
+                if self._sniffer.running:
+                    self._logger.fatal('%s has been raised by the sniffer but '
+                                       'the sniffer is still running.', err)
+                    raise
+                self._logger.info('%s has been suppressed as the sniffer is not'
+                                  ' running.', err)
 
-        if process.returncode != 0:
-            raise subprocess.CalledProcessError(
-                process.returncode, ' '.join(command), stdout, stderr)
-        return stdout
+            if not self._sniffer.results:
+                self._logger.warning('Sniffing complete but failed to capture '
+                                     'packets [result: %s]', self.results)
+                return
 
-    def pcap(self) -> bytes:
-        """Returns the results in pcap format serialised to bytes."""
-        pcap = ScapyPacketSniffer.to_pcap(self._sniffer.results)
-        if self.snaplen:
-            self._logger.info("Truncating packets to %d bytes", self.snaplen)
-            pcap = self._truncate_pcap(pcap)
-        return pcap
-
-    @staticmethod
-    def to_pcap(packet_list: scapy.plist.PacketList) -> bytes:
-        """Encodes the provided packet list in PCAP format."""
-        byte_buffer = io.BytesIO()
-        with scapy.utils.PcapWriter(byte_buffer) as writer:
-            writer.write(packet_list)
-            writer.flush()
-            # PcapWriter will close the bytebuffer so we must return in 'with'
-            return byte_buffer.getvalue()
-
-    def start(self) -> None:
-        """Start capturing packets."""
-        with self._start_condition:
-            self._sniffer.start()
-            notified = self._start_condition.wait_for(lambda: self._started,
-                                                      timeout=5)
-
-        if not notified:
-            raise SnifferStartTimeout()
-        self._logger.info('Began sniffing for traffic with filter "%s"',
-                          self._filter)
-
-    def stop(self) -> None:
-        """Stop capturing packets."""
-        self._logger.info('Waiting %.2fs for sniffer to flush', self.stop_delay)
-        time.sleep(self.stop_delay)
-
-        try:
-            self._sniffer.stop()
-        except OSError as err:
-            if err.errno != 9:
-                raise
-            if self._sniffer.running:
-                self._logger.fatal('%s has been raised by the sniffer but the '
-                                   'sniffer is still running.', err)
-                raise
-            self._logger.info('%s has been suppressed as the sniffer is not '
-                              'running.', err)
-
-        if not self._sniffer.results:
-            self._logger.warning('Sniffing complete but failed to capture '
-                                 'packets [result: %s]', self.results)
-            return
-
-        self._sniffer.results = scapy.plist.PacketList(
-            name='Sniffed',
-            res=self._sniffer.results,
-            stats=[inet.TCP, inet.UDP])
-        self._logger.info('Sniffing complete. %r', self._sniffer.results)
+            self._sniffer.results = scapy.plist.PacketList(
+                name='Sniffed',
+                res=self._sniffer.results,
+                stats=[inet.TCP, inet.UDP])
+            self._logger.info('Sniffing complete. %r', self._sniffer.results)
 
 
 class TCPDumpPacketSniffer(PacketSniffer):
