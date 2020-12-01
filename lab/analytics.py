39ddaf7c20b022bef13a5f8a27d13e72564b74c5
Jean-Pierre Smith <rougesprit@gmail.com>
2020-12-01 09:08:10 +0100

    Add utility functions

--- lab/analytics.py
+++ lab/analytics.py
@@ -1,6 +1,14 @@
-"""Methods useful for analysis of results."""
+"""Methods useful for analysis of results.
+"""
 import itertools
+from typing import NamedTuple
+
+import numpy as np
 import pandas as pd
+from sklearn.utils import check_array
+from typing_extensions import Final
+
+from . import metrics
 
 
 def median_difference(frame: pd.DataFrame, level: str = "") -> pd.DataFrame:
@@ -13,3 +21,61 @@ def median_difference(frame: pd.DataFrame, level: str = "") -> pd.DataFrame:
     for lhs, rhs in itertools.combinations(level_values, 2):
         result_columns[f"({lhs} - {rhs})"] = medians.loc[lhs] - medians.loc[rhs]
     return pd.DataFrame(result_columns).T
+
+
+PRCurveResult = NamedTuple("PRCurveResult", [
+    ("precision", np.ndarray), ("recall", np.ndarray),
+    ("thresholds", np.ndarray)
+])
+
+
+# pylint: disable=too-many-arguments
+def rprecision_recall_curve(
+    y_true,
+    probas_pred,
+    ratio: float = 20,
+    neg_label=-1,
+    pos_labels=None,
+) -> PRCurveResult:
+    """Calculation of the precision recall scores for various thresholds
+    in multiclass classification using rprecision.
+
+    pos_labels correspond to the columns of probas_pred, assumed to be
+    [0, n_classes] if not specified.
+
+    Predictions falling below the decision thresholds are classified
+    as neg_label.
+    """
+    y_true = check_array(y_true, dtype=int, ensure_2d=False)
+    probas_pred = check_array(probas_pred)
+    pos_labels = check_array(pos_labels, dtype=int, ensure_2d=False) \
+        if pos_labels else np.arange(probas_pred.shape[1])
+    assert len(pos_labels) == probas_pred.shape[1]
+    assert neg_label not in pos_labels
+
+    y_pred: Final = pos_labels[np.argmax(probas_pred, axis=1)]
+    max_probabilities: Final = np.max(probas_pred, axis=1)
+
+    thresholds = np.unique(np.around(max_probabilities, decimals=3))
+
+    precision = np.zeros(len(thresholds))
+    recall = np.zeros(len(thresholds))
+
+    for i, threshold in enumerate(thresholds):
+        below_threshold = (max_probabilities < threshold)
+
+        # Dont modify y_pred since it is reused
+        y_pred_thresh = y_pred.copy()
+        y_pred_thresh[below_threshold] = neg_label
+
+        precision[i] = metrics.rprecision_score(
+            y_true, y_pred_thresh, ratio=ratio, zero_division=1,
+            negative_class=neg_label)
+        recall[i] = metrics.recall_score(
+            y_true, y_pred_thresh, negative_class=neg_label)
+
+        if precision[i] == 0.0 and recall[i] == 0.0:
+            raise RuntimeError(
+                f"Precision and recall both zero at threshold {threshold}")
+
+    return PRCurveResult(precision, recall, thresholds)
--- lab/metrics.py
+++ lab/metrics.py
@@ -1,5 +1,9 @@
 """Additional classification metrics."""
 import logging
+import warnings
+
+from typing_extensions import Literal
+import numpy as np
 
 
 def recall_score(y_true, y_pred, negative_class=-1):
@@ -8,14 +12,15 @@ def recall_score(y_true, y_pred, negative_class=-1):
     """
     pos_labels = (y_true != negative_class)
 
-    n_true_positive = sum(pos_labels & (y_true == y_pred))
-    n_positive = sum(pos_labels)
+    n_true_positive = np.sum(pos_labels & (y_true == y_pred))
+    n_positive = np.sum(pos_labels)
 
     return n_true_positive / n_positive
 
 
 def rprecision_score(
-    y_true, y_pred, ratio: float = 1.0, negative_class=-1
+    y_true, y_pred, ratio: float = 1.0, negative_class=-1,
+    zero_division: Literal["warn", 0, 1] = "warn"
 ):
     """Calculate r-precision score for multiclass classification.
 
@@ -31,22 +36,23 @@ def rprecision_score(
 
     for more information.
     """
+    # pylint: disable=too-many-locals
     logger = logging.getLogger(__name__)
 
     pos_labels = (y_true != negative_class)
     pos_predictions = (y_pred != negative_class)
 
-    n_true_positive = sum(pos_labels & (y_true == y_pred))
+    n_true_positive = np.sum(pos_labels & (y_true == y_pred))
     logger.debug("n_true_positive: %d", n_true_positive)
 
     # Positive predictions which were not correct for positive classes
-    n_wrong_positive = sum(pos_labels & pos_predictions & (y_true != y_pred))
-    n_false_positive = sum(~pos_labels & pos_predictions)
+    n_wrong_positive = np.sum(pos_labels & pos_predictions & (y_true != y_pred))
+    n_false_positive = np.sum(~pos_labels & pos_predictions)
     logger.debug("n_wrong_positive: %d, n_false_positive: %d",
                  n_wrong_positive, n_false_positive)
 
-    n_positive = sum(pos_labels)
-    n_negative = sum(~pos_labels)
+    n_positive = np.sum(pos_labels)
+    n_negative = len(y_true) - n_positive
     logger.debug("n_positive: %d, n_negative: %d", n_positive, n_negative)
 
     true_positive_rate = n_true_positive / n_positive
@@ -54,7 +60,11 @@ def rprecision_score(
     false_positive_rate = n_false_positive / n_negative
 
     if n_true_positive == n_wrong_positive == n_false_positive == 0:
-        return 0
+        if zero_division == "warn":
+            warnings.warn("Attempted division by zero in rprecision. "
+                          "Returning 0 instead.", RuntimeWarning)
+            zero_division = 0
+        return zero_division
 
     logger.debug("r_%d-precision = %.3g / (%.3g + %.3g + %d * %.3g)",
                  ratio, true_positive_rate, true_positive_rate,
